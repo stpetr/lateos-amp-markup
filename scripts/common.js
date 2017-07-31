ec2305b3ae4860c22f1f9ef8c732578d1b130abe
Petr Dvukhrechensky <petr.sdkb@gmail.com>
2017-07-31 10:07:14 +0700

    Added Territory mappings page

--- scripts/common.js
+++ scripts/common.js
@@ -1,5 +1,26 @@
 $(function() {
+    // Apply `Slick` slider
     $('.slider').slick({
         dots: true,
     });
+
+    // Handle slider with menu
+    (function() {
+        var $slider = $('.slider.with-menu');
+
+        // Add `before-active` class on load
+        $slider.parent().find('.slider-menu:first').children('.active').prev().addClass('before-active');
+
+        // Add `before-active` class on slide change
+        $slider.on('beforeChange', function(e, slick, currentSlide, nextSlide) {
+            var $menu = $(this).parent().find('.slider-menu:first');
+
+            $menu.children('.active').removeClass('active before-active');
+            $menu.children().eq(nextSlide).addClass('active');
+
+            if (nextSlide > 0) {
+                $menu.children().eq(nextSlide - 1).addClass('before-active');
+            }
+        });
+    })();
 });
